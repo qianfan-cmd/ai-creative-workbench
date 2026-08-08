be17f1757fd4d7dd600c2e57f592efdffd0590ce
yifan.qian <3491116167@qq.com>
2026-08-08 18:56:21 +0800

    feat:静态映射

--- backend-java/src/main/java/com/workbench/backendjava/config/WebMvcConfig.java
+++ backend-java/src/main/java/com/workbench/backendjava/config/WebMvcConfig.java
@@ -31,6 +31,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 );
     }
 
+    /**
+     * 映射上传的静态资源
+     * ResourceHandlerRegistry用来注册[URL->文件夹]映射
+     * @param registry
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         /**
@@ -40,7 +45,27 @@ public class WebMvcConfig implements WebMvcConfigurer {
          *     ↓
          * 避免 Windows 下 file:./ 解析偏差
          */
+        /**
+         * 把配置目录转成绝对Path
+         * 步骤	含义
+         * uploadProperties.getDir()
+         * 从 yml 读 ./uploads
+         * Paths.get(...)
+         * 字符串 → Path
+         * toAbsolutePath()
+         * 相对路径 → 绝对路径，避免 Tomcat 临时目录偏差
+         * normalize()
+         * 去掉 .\、.. 等，路径更干净
+         */
         Path uploadDir = Paths.get(uploadProperties.getDir()).toAbsolutePath().normalize();
+        /**
+         * 转成spring能用的location字符串
+         * spring的addResourceLocation需要类似 file:/E:/ai-creative-workbench/backend-java/uploads/
+         * file: 表示协议，是本地文件系统的意思，后面是绝对路径
+         * 步骤	含义
+         * uploadDir.toUri()
+         *
+         */
         String location = uploadDir.toUri().toString();
 
         // Windows下确保以 / 结尾
@@ -48,7 +73,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
             location = location + "/";
         }
 
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:./uploads/");
+        registry.addResourceHandler("/uploads/**")// URL匹配规则，表示哪些URL当静态资源
+                .addResourceLocations(location);// 磁盘上的真实目录，表示这些URL对应磁盘的哪里
     }
 }
